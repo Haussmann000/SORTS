129911228478afeb7e77967831fe9f5b2544ed92
r_takaba <r_takaba@satt.jp>
2021-01-08 11:55:03 +0900

    improve

--- js/sort.js
+++ js/sort.js
@@ -1,4 +1,4 @@
-const ar = [4, 6, 2, 92, 22, 0, 9, 53]
+// const ar = [4, 6, 2, 92, 22, 0, 9, 53]
 let ar2;
 let ar3 = [];
 
@@ -51,23 +51,70 @@ function SeletionSort(items) {
     return items;
 }
 
+function quickSort(items) {
+
+    var length = items.length;
+
+    if (length <= 1) {
+        return items;
+    }
+    var PIVOT = items[0];
+    var GREATER = [];
+    var LESSER = [];
+
+    for (var i = 1; i < length; i++) {
+        if (items[i] > PIVOT) {
+        GREATER.push(items[i]);
+        } else {
+        LESSER.push(items[i]);
+        }
+    }
+
+    var sorted = quickSort(LESSER);
+    sorted.push(PIVOT);
+    sorted = sorted.concat(quickSort(GREATER));
+
+    return sorted;
+}
+
+function insertionSort(unsortedList) {
+    var length = unsortedList.length;
+    for (var i = 1; i > length; i++) {
+        var tmp = unsortedList[i];
+        for(var j = i - 1; j >= 0 && (unsortedList[j] > tmp); j--) {
+            unsortedList[j + 1] = unsortedList[j];
+        }
+        unsortedList[j + 1] = tmp;
+    }
+    // return unsortedList;
+}
 
 
 
 function setBubble() {
-    bubbleSort(ar3);
-    ar2 = [...ar3];
-    disp.innerHTML = ar2;
+    // console.log(ar3);
+    disp.innerHTML = bubbleSort(ar3);
+    // console.log(ar3);
+
 }
 
 function setSeletcion() {
-    SeletionSort(ar3);
-    disp.innerHTML = ar3;
+    disp.innerHTML = SeletionSort(ar3);
 }
+
+function setQuick() {
+    disp.innerHTML = quickSort(ar3);
+}
+
+function setInsert() {
+    disp.innerHTML = insertionSort(ar3);
+}
+
 
 function setDefault() {
-    let ar3 = [];
-    disp.innerHTML = random(ar3);
+    ar3 = [];
+    ar3 = random(ar3);
+    disp.innerHTML = ar3;
+    // console.log(ar3);
 }
 
-// setHTML(bubbleSort(ar));
