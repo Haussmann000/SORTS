554e0021e026883e83d364894a745b040af7f80b
takaba <r_takaba@satt.jp>
2021-03-05 20:07:37 +0900

    delete 条件入力案

--- js/sort.js
+++ js/sort.js
@@ -1,27 +1,58 @@
-// const ar = [4, 6, 2, 92, 22, 0, 9, 53]
-let ar2;
-let ar3 = [];
-
+let array = [];
+const arrayNumber = 7;
+const maxNumber = 100;
 const start = document.querySelector("#start");
 const disp = document.querySelector(".disp_area");
+const title = document.querySelector(".title_area");
+const titleCharacter = ["S", "O", "R", "T", "S", "!"];
 
-// console.log(ar)     //[4, 6, 2, 92, 22, 0, 9, 53]
-// console.log(ar)     //[0, 2, 4, 6, 9, 22, 53, 92]
-disp.innerHTML = random(ar3);
-
-function random(ar3) {
-    const integers = Array.from(Array(100).keys())
-    for(let i = 0; i < 6; i++) {
-        var rand = Math.floor(Math.random() * (100));
-        ar3.push(rand);
+const createRandom = (array) => {
+    const integers = Array.from(Array(maxNumber).keys())
+    for(let i = 0; i < arrayNumber; i++) {
+        var rand = Math.floor(Math.random() * (maxNumber));
+        array.push(rand);
     }
-    return ar3;
+    return array;
 }
 
 
+const createSpan = (items, className = "colors") => {
+    disp.innerHTML = "";
+    items.forEach(e => {
+        let child = document.createElement("span")
+        child.innerHTML = e;
+        disp.appendChild(child).setAttribute("class", className);
+    })
+    return disp;
+}
+
+const initTitle = () => {
+    disp.innerHTML = createRandom(array);
+    createSpan(titleCharacter, "colors");
+}
+
+const sortFunction = method =>{
+    const start = performance.now();
+    method(array);
+    const end = performance.now();
+    console.log(end - start);
+    createSpan(array);
+};
+
+
+const setDefault = () => {
+    array = [];
+    array = createRandom(array);
+    createSpan(array, "resetcolor");
+}
+
+
+//// ソート
+
+// Bubble Sort
 function bubbleSort(items){
     let length = items.length;
-    for (let i = (length -1); i > 0; i--){
+    for (let i = (length - 1); i > 0; i--){
         for (let j = (length - i); j > 0; j--){
             if(items[j] < items[j - 1]){
                 let tmp = items[j];
@@ -33,7 +64,9 @@ function bubbleSort(items){
     return items;
 }
 
-function SeletionSort(items) {
+
+// Selection Sort
+const selectionSort = (items) => {
     var length = items.length;
     for (var i = 0; i < length - 1; i++) {
         var min  = i;
@@ -51,7 +84,8 @@ function SeletionSort(items) {
     return items;
 }
 
-function quickSort(items) {
+// Quick Sort
+const quickSort = (items) => {
 
     var length = items.length;
 
@@ -77,43 +111,59 @@ function quickSort(items) {
     return sorted;
 }
 
+// Insertion Sort
 function insertionSort(unsortedList) {
-  var len = unsortedList.length;
-  for (var i = 1; i < len; i++) {
-    var tmp = unsortedList[i];
-    for (var j = i - 1; j >= 0 && (unsortedList[j] > tmp); j--) {
-      unsortedList[j + 1] = unsortedList[j];
+    var len = unsortedList.length;
+    for (var i = 1; i < len; i++) {
+        var tmp = unsortedList[i];
+        for (var j = i - 1; j >= 0 && (unsortedList[j] > tmp); j--) {
+            unsortedList[j + 1] = unsortedList[j];
+        }
+        unsortedList[j + 1] = tmp;
     }
-    unsortedList[j + 1] = tmp;
-  }
+}
+
+// Shell Sort
+const shellSort = (items) => {
+    var interval = 1;
+    while (interval < items.length / 3){
+        interval = interval * 3 + 1;
+    }
+
+    while(interval > 0) {
+        for (var outer = interval; outer < items.length; outer++) {
+            var value  = items[outer];
+            var inner = outer;
+            while(inner > interval - 1 && items[inner - interval] >= value) {
+                items[inner] = items[inner - interval];
+                inner = inner - interval;
+            }
+            items[inner] = value;
+        }
+        interval = (interval -1) / 3;
+    }
+    return items;
+};
+
+// Counting Sort
+const countingSort = (arr, min = 1, max = maxNumber) => {
+    let i;
+    let z = 0;
+    const count = [];
+    for (i = min; i <= max; i++) {
+        count[i] = 0;
+    }
+    console.log(count[i]);
+    for (i = 0; i < arr.length; i++) {
+        count[arr[i]]++;
+    }
+    console.log(count[i]);
+    for (i = min; i <= max; i++) {
+        while (count[i]-- > 0) {
+            arr[z++] = i;
+        }
+    }
 }
 
 
-
-function setBubble() {
-    // console.log(ar3);
-    disp.innerHTML = bubbleSort(ar3);
-    // console.log(ar3);
-
-}
-
-function setSeletcion() {
-    disp.innerHTML = SeletionSort(ar3);
-}
-
-function setQuick() {
-    disp.innerHTML = quickSort(ar3);
-}
-
-function setInsert() {
-    disp.innerHTML = insertionSort(ar3);
-}
-
-
-function setDefault() {
-    ar3 = [];
-    ar3 = random(ar3);
-    disp.innerHTML = ar3;
-    // console.log(ar3);
-}
-
+window.addEventListener('DOMContentLoaded', initTitle());
